027a7d4d774110be37b81fb514d3f30da7f60afe
nad <nad@nads-MacBook-Pro.local>
2018-03-19 14:50:27 +0300

    Initial Commit

---
+++ todoey2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todoey2
+//
+//  Created by nad on 7/2/1439 AH.
+//  Copyright © 1439 nad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
